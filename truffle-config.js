0ae98919817995e762084389498e355c7bd832f2
harveys995 <92803332+harveys995@users.noreply.github.com>
2022-09-28 18:23:07 +0100

    Final VPA Project

--- truffle-config.js
+++ truffle-config.js
@@ -20,24 +20,4 @@ module.exports = {
       version: "^0.8.0",
     },
   },
-
-  // networks: {
-  //   ropsten: {
-  //     provider: () =>
-  //       new HDWalletProvider(
-  //         mnemonic,
-  //         `https://speedy-nodes-nyc.moralis.io/2bf203d7cf1c94c884b09a06/eth/ropsten`
-  //       ),
-  //     network_id: 3, // Ropsten's id
-  //     gas: 5500000, // Ropsten has a lower block limit than mainnet
-  //     confirmations: 2, // # of confs to wait between deployments. (default: 0)
-  //     timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-  //     skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
-  //   },
-  // },
 };
-
-// const HDWalletProvider = require("@truffle/hdwallet-provider");
-// //
-// // const fs = require('fs');
-// const mnemonic = require("./secrets.json").mnemonic;
